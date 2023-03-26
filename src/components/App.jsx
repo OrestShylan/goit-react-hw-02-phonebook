6678b3a1e640e056aa84e2f607ae0ed9d0f51b74
OrestShylan <112808022+OrestShylan@users.noreply.github.com>
2023-03-26 17:41:36 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+    number: '',
+  };
+
+  handleAddContact = newContact =>
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+    }));
+
+  handleCheckName = name => {
+    const { contacts } = this.state;
+    const isExistContact = !!contacts.find(contact => contact.name === name);
+    isExistContact && alert(`${name} already in contacts!`);
+    return !isExistContact;
+  };
+
+  handleRemoveContact = id =>
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
+    }));
+
+  handleFilterChange = filter => this.setState({ filter });
+  getContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filterContacts = this.getContacts();
+
+    return (
+      <div className="formSection">
+        <h1> Phone book </h1>
+        <ContactForm
+          onAdd={this.handleAddContact}
+          checkName={this.handleCheckName}
+        />
+        <h2> Contacts </h2>
+
+        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filterContacts}
+          onRemove={this.handleRemoveContact}
+        />
+      </div>
+    );
+  }
+}
